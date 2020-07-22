427a2bc808df5ca6e03907bb9362934b95587756
Cosmo <53384583+immortalcosmo@users.noreply.github.com>
2020-07-22 19:39:26 -0400

    Update plotCounty.py

--- plotCounty.py
+++ plotCounty.py
@@ -6,18 +6,19 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
     counties = json.load(response)
 
 import plotly.express as px
-df = pd.read_csv('county.csv', index_col="date")
-date_index = pd.date_range(start='2/1/2020', end='6/30/2020') #From February to the end of June
+df = pd.read_csv('county.csv', index_col="date", dtype ={'county_fips_code': 'str'})
+date_index = pd.date_range(start='3/1/2020', end='7/21/2020') #Starting from March
 for date in date_index:
     fig = px.choropleth(df.loc[date.strftime("%Y-%m-%d")], geojson=counties, locations='county_fips_code',
                     color='confirmed_cases_per_100000',
 
-                    color_continuous_scale="Viridis",
-                    range_color=(0, 1000), #Max color range is therefore 1% of total population
+                    color_continuous_scale="YlOrRd",
+                    range_color=(0, 2000), #Max color range is therefore 2% of total population
                     scope="usa",
-                    labels={'confirmed_cases_per_100000': 'Cases per 100,000'}
+                    labels={'confirmed_cases_per_100000': 'Cases per 100,000'},
                     )
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.update_layout(autosize=False,
+                      title_text = "COVID 19 - U.S Counties<br><br>" + date.strftime("%b %d, %Y"))
     fig.write_image("images/" + date.strftime("%Y-%m-%d") + ".png")
     break
     #Uncomment break to generate all figures, will take a few minutes.
