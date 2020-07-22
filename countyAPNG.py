8134922178395d90c1aeb4690166b05f10c46b08
Cosmo <53384583+immortalcosmo@users.noreply.github.com>
2020-07-22 19:41:44 -0400

    Generates animated png from plotCounty figures

---
+++ countyAPNG.py
@@ -0,0 +1,11 @@
+from apng import APNG
+import pandas as pd
+date_index = pd.date_range(start='3/1/2020', end='7/21/2020') #From February to the end of June
+files = []
+for date in date_index:
+    files.append(("images/" + date.strftime("%Y-%m-%d")+".png", 200))
+print(files)
+im = APNG()
+for file, delay in files:
+  im.append_file(file, delay=delay)
+im.save("AnimatedCounties.png")
