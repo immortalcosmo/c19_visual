530085c95bbf53695cd9e7472f7a39218b23ef92
Cosmo <53384583+immortalcosmo@users.noreply.github.com>
2020-07-23 16:52:56 -0400

    Update countyAPNG.py

--- countyAPNG.py
+++ countyAPNG.py
@@ -1,6 +1,6 @@
 from apng import APNG
 import pandas as pd
-date_index = pd.date_range(start='3/1/2020', end='7/21/2020') #From February to the end of June
+date_index = pd.date_range(start='3/1/2020', end='7/21/2020') 
 files = []
 for date in date_index:
     files.append(("images/" + date.strftime("%Y-%m-%d")+".png", 200))
